a0172c9240bc9033da914d1fa2d87350d815e32d
Francisco Miranda <franciscomiranda.inbox@gmail.com>
2022-09-28 17:30:27 -0300

    added global provider widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:konsi_challenge/src/konsi_challenge_app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  runApp(const KonsiChallengeApp());
 }
---
+++ lib/src/konsi_challenge_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class KonsiChallengeApp extends StatelessWidget {
+  const KonsiChallengeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'KonsiChallenge App',
+      home: Container(),
+    );
+  }
+}

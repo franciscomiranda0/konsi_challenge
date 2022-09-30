df08e7aa7d32cca11158b0f18769897b9bfac657
Francisco Miranda <franciscomiranda.inbox@gmail.com>
2022-09-30 09:53:41 -0300

    added cep entity to domain layer

---
+++ lib/src/domain/entities/cep.dart
@@ -0,0 +1,15 @@
+class CEP {
+  final String code;
+  final String state;
+  final String city;
+  final String neighborhood;
+  final String street;
+
+  const CEP({
+    required this.code,
+    required this.state,
+    required this.city,
+    required this.neighborhood,
+    required this.street,
+  });
+}

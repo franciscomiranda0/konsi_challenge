3392c86ee8529ee8ac1ab0875ea7b7f21bcde240
Francisco Miranda <franciscomiranda.inbox@gmail.com>
2022-09-30 09:27:29 -0300

    set .env file load in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:konsi_challenge/src/konsi_challenge_app.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
+
   runApp(const KonsiChallengeApp());
 }

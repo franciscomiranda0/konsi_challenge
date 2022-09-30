92625a13e44372d0e55af59dcbe53bef1f50322f
Francisco Miranda <franciscomiranda.inbox@gmail.com>
2022-09-30 17:59:01 -0300

    added flutter_hooks; implemented scaffold structure of CepManagerPage

--- lib/src/presentation/pages/cep_manager_page/cep_manager_page.dart
+++ lib/src/presentation/pages/cep_manager_page/cep_manager_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class CepManagerPage extends StatelessWidget {
+class CepManagerPage extends HookWidget {
+  static const _views = [SizedBox(), SizedBox()];
+
   const CepManagerPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final selectedViewIndex = useState(0);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Gerenciador de CEPs')),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedViewIndex.value,
+        onTap: (int index) => selectedViewIndex.value = index,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Caderneta',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Pesquisa',
+          ),
+        ],
+      ),
+      body: _views[selectedViewIndex.value],
+    );
   }
 }

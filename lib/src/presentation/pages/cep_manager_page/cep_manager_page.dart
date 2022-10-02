8ddaa4367623b325fc98e7edc6dbd904b20bfa81
Francisco Miranda <franciscomiranda.inbox@gmail.com>
2022-10-02 01:07:48 -0300

    cleaned some code grouping related data; improved readability

--- lib/src/presentation/pages/cep_manager_page/cep_manager_page.dart
+++ lib/src/presentation/pages/cep_manager_page/cep_manager_page.dart
@@ -5,26 +5,27 @@ import 'package:konsi_challenge/src/presentation/pages/cep_manager_page/widgets/
 class CepManagerPage extends HookWidget {
   const CepManagerPage({Key? key}) : super(key: key);
 
+  void _setView({
+    required int index,
+    required ValueNotifier<ViewState> viewState,
+  }) =>
+      viewState.value = index == 0
+          ? const NotebookViewState(selectedIndex: 0)
+          : const SearchViewState(selectedIndex: 1);
+
   @override
   Widget build(BuildContext context) {
-    final selectedIndex = useState(0);
-    final indexView = useState<Widget>(const NotebookView());
-    final indexColor = useState(Colors.amber);
+    final viewState = useState<ViewState>(
+      const NotebookViewState(selectedIndex: 0),
+    );
 
     return Theme(
-      data: ThemeData(primarySwatch: indexColor.value),
+      data: ThemeData(primarySwatch: viewState.value.primarySwatch),
       child: Scaffold(
         appBar: AppBar(title: const Text('Gerenciador de CEPs')),
         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: selectedIndex.value,
-          onTap: (int index) {
-            final isIndex0 = index == 0;
-
-            selectedIndex.value = index;
-            indexView.value =
-                isIndex0 ? const NotebookView() : const SizedBox();
-            indexColor.value = isIndex0 ? Colors.blue : Colors.green;
-          },
+          currentIndex: viewState.value.selectedIndex,
+          onTap: (index) => _setView(index: index, viewState: viewState),
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.book),
@@ -36,8 +37,36 @@ class CepManagerPage extends HookWidget {
             ),
           ],
         ),
-        body: indexView.value,
+        body: viewState.value.view,
       ),
     );
   }
+}
+
+abstract class ViewState {
+  final Widget view;
+  final MaterialColor primarySwatch;
+  final int selectedIndex;
+
+  const ViewState({
+    required this.view,
+    required this.primarySwatch,
+    required this.selectedIndex,
+  });
+}
+
+class NotebookViewState extends ViewState {
+  const NotebookViewState({required super.selectedIndex})
+      : super(
+          primarySwatch: Colors.blue,
+          view: const NotebookView(),
+        );
+}
+
+class SearchViewState extends ViewState {
+  const SearchViewState({required super.selectedIndex})
+      : super(
+          primarySwatch: Colors.green,
+          view: const SizedBox(),
+        );
 }

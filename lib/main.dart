3437700e9d9f51afbe23d0d861ae6c6a7cff7a85
Francisco Miranda <franciscomiranda.inbox@gmail.com>
2022-10-03 10:10:50 -0300

    implemented get_it for dependecy injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:konsi_challenge/src/core/injection/injector.dart';
 import 'package:konsi_challenge/src/core/utils/constants.dart';
 import 'package:konsi_challenge/src/konsi_challenge_app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: Constants.dotEnvPath);
+  await initializeDependencies();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   runApp(const KonsiChallengeApp());
 }
---
+++ lib/src/core/injection/injector.dart
@@ -0,0 +1,15 @@
+import 'package:get_it/get_it.dart';
+import 'package:konsi_challenge/src/data/datasources/remote_datasource.dart';
+import 'package:konsi_challenge/src/data/repositories/cep_repository.dart';
+import 'package:konsi_challenge/src/domain/repositories/cep_repository_interface.dart';
+import 'package:konsi_challenge/src/domain/usecases/get_cep_usecase.dart';
+import 'package:konsi_challenge/src/presentation/blocs/cep_search/cep_search_bloc.dart';
+
+final injector = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  injector.registerSingleton<RemoteDatasourceInterface>(RemoteDatasource());
+  injector.registerSingleton<CepRepositoryInterface>(CepRepository(injector()));
+  injector.registerSingleton<GetCepUseCase>(GetCepUseCase(injector()));
+  injector.registerFactory<CepBloc>(() => CepBloc(injector()));
+}

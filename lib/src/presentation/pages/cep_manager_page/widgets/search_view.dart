e17b72be22ae0cdffff962bdbd02e507cfaece54
Francisco Miranda <franciscomiranda.inbox@gmail.com>
2022-10-02 22:47:57 -0300

    added animation for better aligning content to available size of view for each state

--- lib/src/presentation/pages/cep_manager_page/widgets/search_view.dart
+++ lib/src/presentation/pages/cep_manager_page/widgets/search_view.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:konsi_challenge/src/presentation/widgets/konsi_widgets.dart';
 
-class SearchView extends StatelessWidget {
+class SearchView extends HookWidget {
   const SearchView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final viewHeight = MediaQuery.of(context).size.height;
+    final hasSearched = useState(false);
+
     return SingleChildScrollView(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           const KonsiVerticalSpacer(16),
+          AnimatedContainer(
+            curve: Curves.easeInOut,
+            duration: const Duration(milliseconds: 1250),
+            height: hasSearched.value ? viewHeight * .1 : viewHeight * .3,
+          ),
           TextFormField(),
           KonsiPrimaryButton(
-            onPressed: () {},
+            onPressed: () {
+              hasSearched.value = !hasSearched.value;
+            },
             child: const Text('PESQUISAR'),
           ),
         ],

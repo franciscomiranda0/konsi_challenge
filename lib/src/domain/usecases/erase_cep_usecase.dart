d2c692ba09c96582ad24924a80b354b1478cf5d0
Francisco Miranda <franciscomiranda.inbox@gmail.com>
2022-10-04 15:09:37 -0300

    implemented usecase for erasing cep

--- lib/src/domain/usecases/erase_cep_usecase.dart
+++ lib/src/domain/usecases/erase_cep_usecase.dart
@@ -2,13 +2,13 @@ import 'package:konsi_challenge/src/core/usecases/usecase.dart';
 import 'package:konsi_challenge/src/domain/entities/cep.dart';
 import 'package:konsi_challenge/src/domain/repositories/cep_repository_interface.dart';
 
-class SaveCepUseCase extends UseCase<void, Cep> {
+class EraseCepUseCase extends UseCase<void, Cep> {
   final CepRepositoryInterface _cepRepository;
 
-  SaveCepUseCase(this._cepRepository);
+  EraseCepUseCase(this._cepRepository);
 
   @override
   Future<void> call({required Cep params}) {
-    return _cepRepository.saveCep(params);
+    return _cepRepository.eraseCep(params);
   }
 }

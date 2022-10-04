4225a4f0cb4972ab9de2ee5a28bf85d52beb04ad
Francisco Miranda <franciscomiranda.inbox@gmail.com>
2022-10-04 13:35:30 -0300

    isolated cep table name in constants

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -2,5 +2,6 @@ class Constants {
   const Constants._();
 
   static const String appTitle = 'KonsiChallenge App';
+  static const String cepTableName = 'ceps';
   static const String dotEnvPath = 'assets/env/.env';
 }
--- lib/src/data/datasources/local_datasource/daos/cep_dao.dart
+++ lib/src/data/datasources/local_datasource/daos/cep_dao.dart
@@ -1,9 +1,10 @@
 import 'package:floor/floor.dart';
+import 'package:konsi_challenge/src/core/utils/constants.dart';
 import 'package:konsi_challenge/src/domain/entities/cep.dart';
 
 @dao
 abstract class CepDao {
-  @Query('SELECT * FROM ceps')
+  @Query('SELECT * FROM ${Constants.cepTableName}')
   Future<List<Cep>> getAllCeps();
 
   @Insert(onConflict: OnConflictStrategy.replace)

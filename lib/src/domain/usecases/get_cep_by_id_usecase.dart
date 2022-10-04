3443eeb3487e4a376efe9bf26f996c535d6654bc
Francisco Miranda <franciscomiranda.inbox@gmail.com>
2022-10-04 15:05:36 -0300

    implemented usecase for saved cep by id

---
+++ lib/src/domain/usecases/get_cep_by_id_usecase.dart
@@ -0,0 +1,15 @@
+import 'package:konsi_challenge/src/core/usecases/usecase.dart';
+import 'package:konsi_challenge/src/domain/entities/cep.dart';
+import 'package:konsi_challenge/src/domain/repositories/cep_repository_interface.dart';
+
+class GetCepByIdUseCase extends UseCase<Cep, int> {
+  final CepRepositoryInterface _cepRepository;
+
+  GetCepByIdUseCase(this._cepRepository);
+
+  @override
+  Future<Cep> call({required int params}) {
+    return _cepRepository.getCepById(params);
+  }
+
+}
